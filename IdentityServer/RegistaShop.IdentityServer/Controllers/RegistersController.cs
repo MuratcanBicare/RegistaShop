089419afd285716cfee28d8a3d3e56b500aebbd0
Muratcan Biçare <muratcanbicare@hotmail.com>
2024-06-12 21:35:28 +0300

    IdentityServer added

---
+++ IdentityServer/RegistaShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RegistaShop.IdentityServer.Dtos;
+using RegistaShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace RegistaShop.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegistersController : ControllerBase
+	{
+
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public RegistersController(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+		{
+
+			var value = new ApplicationUser()
+			{
+				UserName = userRegisterDto.Username,
+				Email = userRegisterDto.Email,
+				Name = userRegisterDto.Name,
+				Surname = userRegisterDto.Surname
+			};
+
+			var result = await _userManager.CreateAsync(value, userRegisterDto.Password);
+
+			if (result.Succeeded)
+			{
+				return Ok("Kullanıcı başarılı şekilde oluşturuldu.");
+			}
+			else
+			{
+				return Ok("Bir hata oluştu tekrar deneyiniz.");
+			}
+		}
+	}
+}
