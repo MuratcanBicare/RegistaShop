85b99d048ff67d9b82fe39f821ea2dbb52eb67d5
Muratcan Biçare <muratcanbicare@hotmail.com>
2025-06-17 23:44:18 +0300

    Cargo service established

---
+++ Services/Cargo/RegistaShop.Cargo.DataAccessLayer/Concrete/CargoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistaShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistaShop.Cargo.DataAccessLayer.Concrete
+{
+	public class CargoContext : DbContext
+	{
+
+		override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=localhost,1441;initial Catalog=RegistaShopCargoDB;User=sa;Password=123456aA*;");
+		}
+
+		public DbSet<CargoCompany> CargoCompanies { get; set; }
+		public DbSet<CargoDetail> CargoDetails { get; set; }
+		public DbSet<CargoCustomer> CargoCustomers { get; set; }
+		public DbSet<CargoOperation> CargoOperations { get; set; }
+
+	}
+}
