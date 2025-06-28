78d492e6fe459ef792b36b5614ee34e494840b34
Muratcan Biçare <muratcanbicare@hotmail.com>
2025-06-28 18:42:30 +0300

    productlist page done

---
+++ Frontends/RegistaShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListPaginationComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistaShop.WebUI.ViewComponents.ProductListViewComponents
+{
+	public class _ProductListPaginationComponentPartial : ViewComponent
+	{
+
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
