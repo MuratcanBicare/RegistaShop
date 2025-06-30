5f32e3e1e6ec23836a5f04c52ad46ae2560f6c39
Muratcan Biçare <muratcanbicare@hotmail.com>
2025-06-30 22:19:42 +0300

    shoppingcart page done

---
+++ Frontends/RegistaShop.WebUI/ViewComponents/ShoppingCartViewComponent/_ShoppingCartDiscountCouponComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistaShop.WebUI.ViewComponents.ShoppingCartViewComponent
+{
+	public class _ShoppingCartDiscountCouponComponentPartial : ViewComponent
+	{
+
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ Frontends/RegistaShop.WebUI/ViewComponents/ShoppingCartViewComponent/_ShoppingCartProductListComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistaShop.WebUI.ViewComponents.ShoppingCartViewComponent
+{
+	public class _ShoppingCartProductListComponentPartial : ViewComponent
+	{
+
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
